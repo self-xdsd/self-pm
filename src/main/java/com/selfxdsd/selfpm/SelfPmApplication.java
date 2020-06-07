64f91a18f548530660f97eecb4c6dc875062552d
amihaiemil <amihaiemil@gmail.com>
2020-06-07 17:05:58 +0300

    springboot skeleton

---
+++ src/main/java/com/selfxdsd/selfpm/SelfPmApplication.java
@@ -0,0 +1,13 @@
+package com.selfxdsd.selfpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfPmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfxdsd/selfpm/SelfPmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfxdsd.selfpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfPmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
