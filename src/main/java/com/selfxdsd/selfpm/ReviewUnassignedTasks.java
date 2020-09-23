b869b4b5633b7d3bc0d884a6331e7572dc2b5436
amihaiemil <amihaiemil@gmail.com>
2020-09-23 20:09:14 +0300

    #31 ReviewUnassignedTasks every 10min + unit tests

---
+++ src/main/java/com/selfxdsd/selfpm/ReviewUnassignedTasks.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.selfpm;
+
+import com.selfxdsd.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Each PM will periodically review the unassigned tasks from the projects
+ * they manage.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 0.0.2
+ */
+@Component
+public final class ReviewUnassignedTasks {
+
+    /**
+     * The PMs will review the unassigned tasks every 10 minutes.
+     */
+    private static final int EVERY_10_MINUTES = 600000;
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(
+        ReviewUnassignedTasks.class
+    );
+
+    /**
+     * Self's core.
+     */
+    private final Self selfCore;
+
+    /**
+     * Ctor.
+     * @param selfCode Self Core, injected by Spring automatically.
+     */
+    @Autowired
+    public ReviewUnassignedTasks(final Self selfCode) {
+        this.selfCore = selfCode;
+    }
+
+    /**
+     * Every 10 minutes the PMs should verify their
+     * Invitations and accept them.
+     */
+    @Scheduled(fixedRate = EVERY_10_MINUTES)
+    public void reviewUnassignedTasks() {
+        LOG.debug("PMs reviewing their unassigned tasks...");
+        for(final ProjectManager manager : this.selfCore.projectManagers()) {
+            LOG.debug(
+                "PM @" + manager.username()
+                + " reviewing their unassinged tasks..."
+            );
+            for(final Project project : manager.projects()) {
+                LOG.debug(
+                    "Reviewing unassigned tasks from project "
+                    + project.repoFullName() + " at " + project.provider()
+                    + "... "
+                );
+                project.resolve(
+                    new Event() {
+                        @Override
+                        public String type() {
+                            return Type.UNASSIGNED_TASKS;
+                        }
+
+                        @Override
+                        public Issue issue() {
+                            throw new UnsupportedOperationException(
+                                "No Issue in the " + Type.UNASSIGNED_TASKS
+                                + " event."
+                            );
+                        }
+
+                        @Override
+                        public Comment comment() {
+                            throw new UnsupportedOperationException(
+                                "No Comment in the " + Type.UNASSIGNED_TASKS
+                                + " event."
+                            );
+                        }
+
+                        @Override
+                        public Project project() {
+                            return project;
+                        }
+                    }
+                );
+                LOG.debug("Done.");
+            }
+        }
+        LOG.debug("All PMs finished reviewing their unassigned tasks.");
+    }
+
+}
---
+++ src/test/java/com/selfxdsd/selfpm/ReviewUnassignedTasksTestCase.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.selfpm;
+
+import com.selfxdsd.api.*;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Unit tests for {@link ReviewUnassignedTasks}.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 0.0.2
+ */
+public final class ReviewUnassignedTasksTestCase {
+
+    /**
+     * It should work fine if the PM has no assigned Projects.
+     */
+    @Test
+    public void reviewsNoProjects() {
+        final ProjectManager manager = Mockito.mock(ProjectManager.class);
+        final Projects projects = Mockito.mock(Projects.class);
+        Mockito.when(projects.iterator()).thenReturn(
+            new ArrayList<Project>().iterator()
+        );
+        Mockito.when(manager.projects()).thenReturn(projects);
+
+        final ProjectManagers all = Mockito.mock(ProjectManagers.class);
+        Mockito.when(all.iterator()).thenReturn(
+            Arrays.asList(manager).iterator()
+        );
+        final Self core = Mockito.mock(Self.class);
+        Mockito.when(core.projectManagers()).thenReturn(all);
+        final ReviewUnassignedTasks review = new ReviewUnassignedTasks(core);
+        review.reviewUnassignedTasks();
+        Mockito.verify(core, Mockito.times(1)).projectManagers();
+        Mockito.verify(manager, Mockito.times(1)).projects();
+    }
+
+    /**
+     * It should work fine if the PM has some assigned Projects.
+     */
+    @Test
+    public void reviewsProjects() {
+        final List<Project> mocks = new ArrayList<>();
+        for(int idx = 0; idx <3; idx++){
+            final Project project = Mockito.mock(Project.class);
+            Mockito.doNothing().when(project).resolve(Mockito.any());
+            mocks.add(project);
+        }
+
+        final ProjectManager manager = Mockito.mock(ProjectManager.class);
+        final Projects projects = Mockito.mock(Projects.class);
+        Mockito.when(projects.iterator()).thenReturn(
+            mocks.iterator()
+        );
+        Mockito.when(manager.projects()).thenReturn(projects);
+
+        final ProjectManagers all = Mockito.mock(ProjectManagers.class);
+        Mockito.when(all.iterator()).thenReturn(
+            Arrays.asList(manager).iterator()
+        );
+        final Self core = Mockito.mock(Self.class);
+        Mockito.when(core.projectManagers()).thenReturn(all);
+        final ReviewUnassignedTasks review = new ReviewUnassignedTasks(core);
+        review.reviewUnassignedTasks();
+        Mockito.verify(core, Mockito.times(1)).projectManagers();
+        Mockito.verify(manager, Mockito.times(1)).projects();
+        for(int idx = 0; idx <3; idx++){
+            final Project project = mocks.get(idx);
+            Mockito.verify(project, Mockito.times(1)).resolve(Mockito.any());
+        }
+    }
+}
