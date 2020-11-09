c6629a4b3de6091ae9251f0a97b635e1d6306fb0
cristianpela <cristypela05@gmail.com>
2020-11-09 19:43:27 +0200

    #61 Review contracts marked for removal.

---
+++ src/main/java/com/selfxdsd/selfpm/ReviewContractsMarkedForRemoval.java
@@ -0,0 +1,204 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.selfpm;
+
+import com.selfxdsd.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * Each PM will periodically review the contracts for removal from the projects
+ * they manage.
+ * @author criske
+ * @version $Id$
+ * @since 0.0.2
+ * @todo #63:15min When Contract/Contracts API has a `remove` method, replace
+ *  `project.resolve(this.contractRemovalEvent(contract));` at line 131
+ *  with the appropriate contract removal API call.
+ *  Also remove the temporary helper `contractRemovalEvent()`.
+ */
+@Component
+public final class ReviewContractsMarkedForRemoval {
+
+    /**
+     * The PMs will review contracts every day.
+     */
+    private static final long EVERY_24_HOURS = 86_400_000L;
+
+
+    /**
+     * The PMs will review contracts every day with a start delay of 15 minutes.
+     */
+    private static final long DELAY_15_MINUTES = 900_000L;
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(
+        ReviewContractsMarkedForRemoval.class
+    );
+
+    /**
+     * Self's core.
+     */
+    private final Self selfCore;
+
+    /**
+     * Time "now" supplier that will be used to determined how many days
+     * ago a Contract marked for removal.
+     */
+    private final Supplier<LocalDateTime> nowSupplier;
+
+    /**
+     * Ctor.
+     * @param selfCore Self Core, injected by Spring automatically.
+     */
+    @Autowired
+    public ReviewContractsMarkedForRemoval(final Self selfCore) {
+        this(selfCore, LocalDateTime::now);
+    }
+
+    /**
+     * Ctor used in tests.
+     * @param selfCore Self Core.
+     * @param nowSupplier Time "now" supplier.
+     */
+    ReviewContractsMarkedForRemoval(final Self selfCore,
+                                    final Supplier<LocalDateTime> nowSupplier) {
+        this.selfCore = selfCore;
+        this.nowSupplier = nowSupplier;
+    }
+
+    /**
+     * Every 24 hours the PMs should verify contracts marked for removal.
+     * <br/>
+     * Contracts marked for removal more than 30 days ago will be removed.
+     * <br/>
+     * It also has start of 15 minutes delay, so it will not overlap with
+     * other scheduled jobs.
+     * @checkstyle NestedForDepth (50 lines).
+     */
+    @Scheduled(fixedRate = EVERY_24_HOURS, initialDelay = DELAY_15_MINUTES)
+    public void reviewContractsMarkedForRemoval() {
+        LOG.debug("PMs reviewing their unassigned tasks...");
+        for(final ProjectManager manager : this.selfCore.projectManagers()) {
+            LOG.debug(
+                "PM @" + manager.username()
+                + " reviewing their project contracts marked for removal..."
+            );
+            for(final Project project : manager.projects()) {
+                LOG.debug(
+                    "Reviewing contracts marked for removal from project "
+                    + project.repoFullName() + " at " + project.provider()
+                    + "... "
+                );
+                final List<Contract> toRemove = this.contractsToRemove(project);
+                LOG.debug(
+                    "For project " + project.repoFullName() + " at "
+                        + project.provider() + " there are " + toRemove.size()
+                        + " contracts that will be removed..."
+                );
+                for(final Contract contract: toRemove){
+                    project.resolve(this.contractRemovalEvent(contract));
+                    LOG.debug(
+                        "For project " + project.repoFullName() + " at "
+                            + project.provider() + " contract "
+                            + contract.contractId()
+                            + " was removed..."
+                    );
+                }
+            }
+        }
+        LOG.debug("All PMs finished reviewing their marked for removal "
+            + "project contracts");
+    }
+
+
+    /**
+     * Project contracts that were marked for removal 30+ days ago.
+     * @param project Project.
+     * @return List of contracts to be removed.
+     */
+    private List<Contract> contractsToRemove(final Project project){
+        final LocalDateTime now = this.nowSupplier.get();
+        final Contracts contracts = project.contracts();
+        final List<Contract> toBeRemoved = new ArrayList<>();
+        for (final Contract contract: contracts){
+            final LocalDateTime markedForRemoval = contract.markedForRemoval();
+            if (markedForRemoval != null
+                && markedForRemoval.until(now, ChronoUnit.DAYS) > 30) {
+                toBeRemoved.add(contract);
+            }
+        }
+        return toBeRemoved;
+    }
+
+
+    /**
+     * Note: This method will be removed in the future.
+     * <br/>
+     * Event API used as a hack to test ReviewContractsMarkedForRemoval
+     * by piggy-backing on `project.resolve(...)` until a proper API for
+     * removing a contract exist.
+     * @param contract Contract.
+     * @return Fake Event.
+     */
+    private Event contractRemovalEvent(final Contract contract){
+        return new Event() {
+            @Override
+            public String type() {
+                return "REMOVE_CONTRACT: " + contract.contractId();
+            }
+
+            @Override
+            public Issue issue() {
+                return null;
+            }
+
+            @Override
+            public Comment comment() {
+                return null;
+            }
+
+            @Override
+            public Commit commit() {
+                return null;
+            }
+
+            @Override
+            public Project project() {
+                return null;
+            }
+        };
+    }
+
+}
---
+++ src/test/java/com/selfxdsd/selfpm/ReviewContractsMarkedForRemovalTestCase.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.selfpm;
+
+import com.selfxdsd.api.*;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.List;
+
+/**
+ * Unit tests for {@link ReviewContractsMarkedForRemoval}.
+ * @author criske
+ * @version $Id$
+ * @since 0.0.2
+ */
+public final class ReviewContractsMarkedForRemovalTestCase {
+
+    /**
+     * It should look for project contracts marked for removal and remove
+     * the ones that were marked more than 30 days.
+     * @checkstyle ExecutableStatementCount (100 lines)
+     */
+    @Test
+    public void reviewsAndRemovesContractsMarkedForRemoval() {
+        final LocalDateTime now = LocalDateTime.now();
+
+        final Self self = Mockito.mock(Self.class);
+
+        final ProjectManagers managers = Mockito.mock(ProjectManagers.class);
+        Mockito.when(self.projectManagers()).thenReturn(managers);
+
+        final ProjectManager manager = Mockito.mock(ProjectManager.class);
+
+        final List<ProjectManager> managersSrc = List.of(manager);
+        Mockito.when(managers.iterator())
+            .thenReturn(managersSrc.iterator());
+
+        final Projects projects = Mockito.mock(Projects.class);
+        Mockito.when(manager.projects()).thenReturn(projects);
+
+        final Project project = Mockito.mock(Project.class);
+
+        final List<Project> projectsSrc = List.of(project);
+        Mockito.when(projects.iterator()).thenReturn(projectsSrc.iterator());
+
+        final Contracts contracts = Mockito.mock(Contracts.class);
+        Mockito.when(project.contracts()).thenReturn(contracts);
+        final Contract.Id contractIdA = new Contract.Id(
+            "john/test",
+            "john",
+            "github",
+            "PO"
+        );
+        final Contract.Id contractIdB = new Contract.Id(
+            "john/test",
+            "mark",
+            "github",
+            "DEV"
+        );
+        final Contract.Id contractIdC = new Contract.Id(
+            "john/test",
+            "steve",
+            "github",
+            "DEV"
+        );
+        final List<Contract> contractsSrc = List.of(
+            this.mockContract(contractIdA, null),
+            this.mockContract(contractIdB, this.daysAgo(now, 31)),
+            this.mockContract(contractIdC, this.daysAgo(now, 30))
+        );
+        Mockito.when(contracts.iterator())
+            .thenReturn(contractsSrc.iterator());
+
+        new ReviewContractsMarkedForRemoval(self, () -> now)
+            .reviewContractsMarkedForRemoval();
+
+        Mockito.verify(project, Mockito.times(1))
+            .resolve(Mockito.any());
+    }
+
+    /**
+     * Mocks a Contract for a Project.
+     * @param contractId Contract id.
+     * @param markedForRemoval Date of marking the removal or null.
+     * @return Contract.
+     */
+    private Contract mockContract(final Contract.Id contractId,
+                                  final LocalDateTime markedForRemoval){
+        final Contract contract = Mockito.mock(Contract.class);
+        Mockito.when(contract.contractId()).thenReturn(contractId);
+        Mockito.when(contract.markedForRemoval()).thenReturn(markedForRemoval);
+        return contract;
+    }
+
+    /**
+     * LocalDateTime of days ago from now.
+     * @param now LocalDateTime.
+     * @param days Days.
+     * @return LocalDateTime.
+     */
+    private LocalDateTime daysAgo(final LocalDateTime now, final int days){
+        return now.minus(Period.ofDays(days));
+    }
+}
